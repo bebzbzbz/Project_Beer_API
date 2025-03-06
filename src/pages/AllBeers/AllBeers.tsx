2ce0d6494a03063d816317892315a6c472b199e3
bebzbzbz <balzerbeatrice@gmail.com>
2025-03-06 11:26:35 +0100

    fetch

--- src/pages/AllBeers/AllBeers.tsx
+++ src/pages/AllBeers/AllBeers.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { IBeer } from "../../interfaces/IBeer";
+import axios from "axios";
+
 const AllBeers = () => {
-    return (  <>
-    </>);
+    const [beers, setBeers] = useState<IBeer[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers");
+            console.log(response.data)
+            setBeers(response.data)
+        }
+        fetchData();
+    }, [])
+
+    return (  
+        <>
+
+        </>
+    );
 }
 
 export default AllBeers;
