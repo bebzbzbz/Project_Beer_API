e20eea6d58dacfc383f250f6e735ae9b800860e9
DeborahAleaKoch <alea-de@web.de>
2025-03-06 14:26:07 +0100

    SingleBeerViewCard

--- src/components/SingleCard/SingleCard.tsx
+++ src/components/SingleCard/SingleCard.tsx
@@ -7,10 +7,31 @@ interface BeerProps {
 
 const SingleCard = ({ beer }: BeerProps) => {
 	return (
-		<>
-			<p>Style{beer.name}</p>
-			<Link to={`/allbeers/${beer._id}`}>Details</Link>
-		</>
+		<div
+			className='flex ml-8 mr-10
+		  items-center gap-8 py-10 border-b-1'
+		>
+			<div className='w-1/2'>
+				<img
+					src={beer.image_url}
+					alt={`Image form ${beer.name}`}
+					className=''
+				/>
+			</div>
+			<div>
+				<h1 className='text-3xl text-wrap'>{beer.name}</h1>
+				<p className='text-lg text-wrap text-amber-300 font-black'>
+					{beer.tagline}
+				</p>
+				<p className='text-base my-5'>Created by: {beer.name}</p>
+				<Link
+					to={`/allbeers/${beer._id}`}
+					className='bg-amber-300 rounded-3xl text-white font-medium py-2 px-6'
+				>
+					Details
+				</Link>
+			</div>
+		</div>
 	);
 };
 
