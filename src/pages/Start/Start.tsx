f5096030673d052423738160d71439c093bb6c86
DeborahAleaKoch <alea-de@web.de>
2025-03-06 13:43:24 +0100

    Start and StartCard

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,8 +1,12 @@
+import StartCard from "../../components/StartCard/StartCard";
+
 const Start = () => {
-    return (  
-        <>
-        </>
-    );
-}
+	return (
+		<>
+			<StartCard imgName={"shelf"} linkName={"All Beers"} />
+			<StartCard imgName='glasses' linkName='Random Beer' />
+		</>
+	);
+};
 
 export default Start;
